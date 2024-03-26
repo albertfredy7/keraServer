b3df17aeb9735cc2d4a2ca5b57f9f3fd4fc40571
albertfredy7 <albertfredy007@gmail.com>
2024-03-26 11:51:44 +0530

    kera Server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const jsonServer = require('json-server')
 
 //create json-server
-const restaurantServer = jsonServer.create()
+const keraServer = jsonServer.create()
 
 //create path to json file
 const router = jsonServer.router('db.json')
@@ -11,14 +11,14 @@ const router = jsonServer.router('db.json')
 const middleware = jsonServer.defaults()
 
 //use middleware and router in server
-restaurantServer.use(middleware)
-restaurantServer.use(router)
+keraServer.use(middleware)
+keraServer.use(router)
 
 //setup port for running server
 const port = 5000 || process.env.PORT 
 
 
 //to listen server for resolving request from client
-restaurantServer.listen(port,()=>{
-    console.log(`restaurantserver started at ${port}  and waiting for the request`);
+keraServer.listen(port,()=>{
+    console.log(`KeraServer started at ${port}  and waiting for the request`);
 })
